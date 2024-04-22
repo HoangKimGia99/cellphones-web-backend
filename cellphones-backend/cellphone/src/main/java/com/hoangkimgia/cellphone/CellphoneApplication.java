55deaef2e3e26da44716d64a523a8ec1bb72cd4f
kimgia <hoangkimgia365@gmail.com>
2024-04-22 09:29:52 +0700

    dsgsdg

---
+++ cellphones-backend/cellphone/src/main/java/com/hoangkimgia/cellphone/CellphoneApplication.java
@@ -0,0 +1,13 @@
+package com.hoangkimgia.cellphone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CellphoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CellphoneApplication.class, args);
+	}
+
+}
---
+++ cellphones-backend/cellphone/src/test/java/com/hoangkimgia/cellphone/CellphoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoangkimgia.cellphone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CellphoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
